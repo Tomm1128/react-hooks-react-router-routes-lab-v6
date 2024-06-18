dc213f2961a540eaa9e952c4d6db512f131e22ca
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-18 10:14:36 -0400

    Update directors page with director info from db

--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,16 +1,39 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
+import NavBar from "../components/NavBar"
 
 function Directors() {
+  const [directors, setDirectors] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+      .then((resp) => resp.json())
+      .then((directors) => setDirectors(directors))
+  })
+
+  const directorsList = directors.map((director) => {
+    const moviesList = director.movies.map((movie) => {
+      return <li key={movie}>{movie}</li>
+    })
+
+    return (
+      <article key={director.id}>
+        <h2>{director.name}</h2>
+        <ul>{moviesList}</ul>
+      </article>
+    )
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directorsList}
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Directors;
+export default Directors
