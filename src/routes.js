7432db950198d7eef8f8ccd408a52f94ac366264
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-18 09:12:08 -0400

    Create routes for navbar

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
+import Directors from "./pages/Directors";
 import Home from "./pages/Home"
+import Actors from "./pages/Actors"
 
 const routes = [
   {
     path: "/",
     element: <Home />
+  },
+  {
+    path: "/directors",
+    element: <Directors />
+  },
+  {
+    path: "/actors",
+    element: <Actors />
   }
 ];
 
